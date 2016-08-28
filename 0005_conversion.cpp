80b07da27afd3974459850245d57757a86d99ae0
Teza Meru <admin@sternsaenger.de>
2016-08-28 22:49:43 +0200

    Update 0005_conversion.cpp

--- 0005_conversion.cpp
+++ 0005_conversion.cpp
@@ -16,5 +16,7 @@ int main() {
 
 //(1)   Wir deklarieren drei werte, ein integer, zwei fließkomma, einer als float und einer als double
 //(2)   dem dritten wert sagen wir einfach das er aus der addition von wert1 und wert2 besteht
-//(3)   Nach der ersten Ausgaben sagen wir aber, das wert3 jetzt aus wert1 besteht, und wert2 das jetzt kein float mehr sein soll, sondern integer
-//(4)   Also sehen wir hier das anstatt 11,3 wie bei der ersten ausgabe nur noch 11 herauskommt, da int keine kommazahlen mitnimmt
+/*(3)   Nach der ersten Ausgaben sagen wir aber, das wert3 jetzt aus wert1 besteht, und wert2 das jetzt ke
+	in float mehr sein soll, sondern integer*/
+/*(4)   Also sehen wir hier das anstatt 11,3 wie bei der ersten ausgabe nur noch 11 herauskommt,
+	da int keine kommazahlen mitnimmt
