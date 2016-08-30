2e506edd89945fd878f4eea71ab9a92be21065ad
Teza Meru <admin@sternsaenger.de>
2016-08-30 08:10:12 +0200

    Create 0009_wertzuweisung.cpp

---
+++ 0009_wertzuweisung.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+
+int main (){
+int mynumber;
+mynumber = 5;     //(1) wert 5 wird zugewiesen
+cout << mynumber; //(2) 5 wird im programm dargestellt
+cout << endl;
+mynumber = 12;    //(3) neuer wert von mynumber ist jetzt 12
+cout << mynumber;
+    return 0;
+}
+
+//(1) wert 5 wird zugewiesen
+//(2) 5 wird im programm dargestellt
+//(3) neuer wert von mynumber ist jetzt 12
+
+
+/*
+ *Beispiel zeigt an das der Wert von 
+ *mynumber von 5 auf 12 gesetzt, also neu definiert wird
+ */
